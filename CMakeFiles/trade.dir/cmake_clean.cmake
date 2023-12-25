b924908152e3c91792c94b137ad86f37d0f934e8
YuminJiang <yumin@qianlonhuangxia.mynetworksettings.com>
2023-12-25 03:09:26 -0500

    final version

---
+++ CMakeFiles/trade.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trade.dir/main.cpp.o"
+  "CMakeFiles/trade.dir/main.cpp.o.d"
+  "trade"
+  "trade.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trade.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/trade.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for trade.
+# This may be replaced when dependencies are built.
